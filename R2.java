fe1b93321d2d2f289c9484744e9414c0308e3e54
nawrrot <kontakt@piotrnawrocki.pl>
2019-03-07 19:29:00 +0100

    my solutions for Kattis exercises

---
+++ R2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Task {
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+        int r1 = sc.nextInt();
+        int s = sc.nextInt();
+        int result=(s*2)-r1;
+        System.out.println(result);
+    }
+}
