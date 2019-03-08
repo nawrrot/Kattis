4725a7c1f01aa6ae1e692b524140ead8cab296ed
nawrrot <kontakt@piotrnawrocki.pl>
2019-03-08 16:54:04 +0100

    My solutions for Kattis exercises

---
+++ Sibice.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import static java.lang.Math.*;
+public class Sibice {
+    public static void main(String[] arg){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int W = scan.nextInt();
+        int H = scan.nextInt();
+        int diagonal=W*W+H*H;
+        double buf=sqrt(diagonal);
+        System.out.println(buf);
+        for (int i=0; i<n; i++){
+          int L = scan.nextInt();
+            if(buf>=L){
+                System.out.println("Da");
+            }
+            else{
+                System.out.println("Ne");
+            }
+        }
+
+    }
+}
