26b0ddd07812543de592c9dd6bbdc6a1a4bf75ff
nawrrot <kontakt@piotrnawrocki.pl>
2019-06-13 14:33:47 +0200

    solved Oddities

---
+++ Oddities.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Oddities {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            int x = scanner.nextInt();
+            if (x % 2 == 0) {
+                System.out.println(x + " is even");
+            } else {
+                System.out.println(x + " is odd");
+            }
+        }
+        scanner.close();
+    }
+}
